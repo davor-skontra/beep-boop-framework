0bcbf966a515b815a74397c7c9cae39eefa44f22
Davor Skontra <thekrad23@gmail.com>
2020-02-12 14:37:39 +0200

    Addhandling for latest and initial values to IBroadcaster and implementation.

--- Broadcasting/Broadcaster.cs
+++ Broadcasting/Broadcaster.cs
@@ -5,15 +5,31 @@ namespace Broadcasting
 {
     public interface IBroadcaster<TObserved> : IObservable<TObserved>, IObserver<TObserved>
     {
+        TObserved Latest { get; }
+
+        void OnNext(Func<TObserved, TObserved> predicate);
         IDisposable Subscribe(Action<TObserved> onNext);
     }
 
-    public class Broadcaster<TObserved>: IBroadcaster<TObserved>
+    public class Broadcaster<TObserved> : IBroadcaster<TObserved>
     {
         private readonly List<IObserver<TObserved>> _observers = new List<IObserver<TObserved>>();
 
+        public Broadcaster()
+        {
+        }
+
+        public Broadcaster(TObserved initialValue)
+        {
+            OnNext(initialValue);
+        }
+
         public IDisposable Subscribe(IObserver<TObserved> observer) =>
             new Unsubscriber(_observers, observer);
+
+        public TObserved Latest { get; private set; }
+
+        public void OnNext(Func<TObserved, TObserved> predicate) => OnNext(predicate(Latest));
 
         public IDisposable Subscribe(Action<TObserved> onNext)
         {
@@ -44,13 +60,15 @@ namespace Broadcasting
 
         public void OnNext(TObserved next)
         {
+            Latest = next;
+
             foreach (var observer in _observers)
             {
                 observer.OnNext(next);
             }
         }
 
-        private class Unsubscriber: IDisposable
+        private class Unsubscriber : IDisposable
         {
             private readonly List<IObserver<TObserved>> _observers;
             private readonly IObserver<TObserved> _observer;
@@ -70,6 +88,4 @@ namespace Broadcasting
             }
         }
     }
-
-    
 }
